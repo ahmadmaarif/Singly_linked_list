1e497d74af1b78e9a2d6e3a8c7f294a0c63873a5
Ahmad Maarif <ahmad.maarif.ft21@mail.umy.ac.id>
2022-11-11 19:43:08 +0700

    add bool search

--- Singly_linked_list/Program.cs
+++ Singly_linked_list/Program.cs
@@ -4,73 +4,115 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Singly_linked_list
+namespace Singlly_linked_list
 {
-    //each node consist of the information part and lik to the next 
+
     class Node
     {
         public int rollNumber;
         public string name;
         public Node next;
-
     }
+
     class list
-    { 
+    {
         Node START;
 
         public list()
         {
             START = null;
-
         }
-        public void addNote() //add a node in the list
+        public void addnode() //add a node in the list
         {
-            int nim ;
+            int nim;
             string nm;
-            Console.Write("\nEnter the roll number of the student :");
+            Console.Write("\n Enter the roll number of the student: ");
             nim = Convert.ToInt32(Console.ReadLine());
-            Console.Write("\nEnter the name off the student :");
+            Console.Write("\n Enter the name of the student: ");
             nm = Console.ReadLine();
             Node newnode = new Node();
             newnode.rollNumber = nim;
             newnode.name = nm;
-            //if node to be inserted is the first node 
+            //if the node to be inserted is the first node 
             if (START == null || nim <= START.rollNumber)
             {
-                if ((START != null) && (nim==START.rollNumber))
+                if ((START != null) && (nim == START.rollNumber))
                 {
-                    Console.WriteLine("\nDuplicate roll numbers not allowed \n");
+                    Console.WriteLine("\n Duplicate roll numbers not allowed\n");
                     return;
-                    {
-                        newnode.next = START;
-                        START = newnode;
-                        return;
-         
-                    }
-                    //Located the position of the new node in the list 
-                    Node previous, current;
-                    previous = START;
-                    current = START;
-
-                    while ((current != null) && (nim >= current.rollNumber))
-                    {
-                        if (nim == current.rollNumber)
-                        {
-                            Console.WriteLine("\n Duplicate roll number not allowed\n");
-                            return;
-                        }
-                        previous = current;
-                        current = current.next;
-                    }
-
-                    /*once the above for loop is executed, prev and current are positioned in such a 
-                      manner that the positon of the new node is added*/
-                    newnode.next = current;
-                    previous.next = newnode;
                 }
-            
+                newnode.next = START;
+                START = newnode;
+                return;
+            }
+
+            // Locate the position of the new node in the list
+            Node previous, current;
+            previous = START;
+            current = START;
+
+            while ((current != null) && (nim >= current.rollNumber))
+            {
+                if (nim == current.rollNumber)
+                {
+                    Console.WriteLine("\n Duplicate roll number not allowed\n");
+                    return;
+                }
+                previous = current;
+                current = current.next;
+            }
+
+            /*once the above for loop is executed, prev and current are positioned in such a 
+              manner that the positon of the new node is added*/
+            newnode.next = current;
+            previous.next = newnode;
+        }
+
+        public void traverse()
+        {
+            if (listEmpty())
+                Console.WriteLine("\n List is empty");
+            else
+            {
+                Console.WriteLine("\n The recordes in the list are : ");
+                Node currentNode;
+                for (currentNode = START; currentNode != null;
+                    currentNode = currentNode.next)
+
+                    Console.Write(currentNode.rollNumber + "   " + currentNode.name + "\n");
+                Console.WriteLine();
             }
         }
+
+        public bool delNode(int nim)
+        {
+            Node previous, current;
+            previous = current = null;
+            // check if the specified node is present in the list or not
+            if (Search(nim, ref previous, ref current) == false)
+                return false;
+            previous.next = current.next;
+            if (current == START)
+                START = START.next;
+            return true;
+        }
+
+        public bool Search(int nim, ref Node previous, ref Node current)
+        {
+            previous = START;
+            current = START;
+            while ((current != null) && (nim != current.rollNumber))
+            {
+                previous = current;
+                current = current.next;
+            }
+            if (current == null)
+                return false;
+            else
+                return true;
+        }
+    
     }
-}
+
    
+}
