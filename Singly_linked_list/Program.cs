ec2b012e5946b74e8ee207a490480d8171ce596d
Ahmad Maarif <ahmad.maarif.ft21@mail.umy.ac.id>
2022-11-11 19:10:44 +0700

    add prev and current are positioned

--- Singly_linked_list/Program.cs
+++ Singly_linked_list/Program.cs
@@ -45,13 +45,31 @@ namespace Singly_linked_list
                         newnode.next = START;
                         START = newnode;
                         return;
-
+         
                     }
+                    //Located the position of the new node in the list 
+                    Node previous, current;
+                    previous = START;
+                    current = START;
+
+                    while ((current != null) && (nim >= current.rollNumber))
+                    {
+                        if (nim == current.rollNumber)
+                        {
+                            Console.WriteLine("\n Duplicate roll number not allowed\n");
+                            return;
+                        }
+                        previous = current;
+                        current = current.next;
+                    }
+
+                    /*once the above for loop is executed, prev and current are positioned in such a 
+                      manner that the positon of the new node is added*/
+                    newnode.next = current;
+                    previous.next = newnode;
                 }
+            
             }
-
-           
-
         }
     }
 }
