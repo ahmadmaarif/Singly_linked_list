82ea7ba3adef305f302a3f3f58f332ac9937a946
Ahmad Maarif <ahmad.maarif.ft21@mail.umy.ac.id>
2022-11-11 17:11:38 +0700

    Add project files.

---
+++ Singly_linked_list/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singly_linked_list
+{
+    //each node consist of the information part and lik to the next mode
+}
+   
