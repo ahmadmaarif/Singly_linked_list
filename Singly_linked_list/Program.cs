22f85aa78fa7b1d89c47744d4bcffc2f98818d20
Ahmad Maarif <ahmad.maarif.ft21@mail.umy.ac.id>
2022-11-11 17:30:38 +0700

    add void addNote

--- Singly_linked_list/Program.cs
+++ Singly_linked_list/Program.cs
@@ -14,7 +14,26 @@ namespace Singly_linked_list
         public Node next;
 
     }
+    class list
+    { 
+        Node START;
 
-    
+        public list()
+        {
+            START = null;
+
+        }
+        public void addNote() //add a node in the list
+        {
+            int nim ;
+            string nm;
+            Console.Write("\nEnter the roll number of the student :");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the name off the student :");
+            
+           
+
+        }
+    }
 }
    
